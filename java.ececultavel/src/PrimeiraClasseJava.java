cc985b7a94fb055547e5b0ac65a7e7e1a0495bd0
Diogo Sousa <sousadiogo453@gmail.com>
2023-05-04 00:14:03 -0300

    Criando o metódo aluno recuperação.

--- java.ececultavel/src/PrimeiraClasseJava.java
+++ java.ececultavel/src/PrimeiraClasseJava.java
@@ -36,14 +36,21 @@ public class PrimeiraClasseJava {
 
         for (int pos = 1; pos <= 4; pos++){
 
-            String nomeDisciplina = JOptionPane.showInputDialog("Nome da disciplina?" +pos+  " ? ");
-            String notaDisciplina = JOptionPane.showInputDialog("Nota da disciplina?" +pos+  " ? ");
+            String nomeDisciplina = JOptionPane.showInputDialog("Nome da disciplina ?" +pos+  " ? ");
+            String notaDisciplina = JOptionPane.showInputDialog("Nota da disciplina ?" +pos+  " ? ");
 
             Disciplina disciplina = new Disciplina();
             disciplina.setDisciplina(nomeDisciplina);
             disciplina.setNota(Double.valueOf(notaDisciplina));
 
             aluno1.getDisciplinas().add(disciplina);
+        }
+
+        int escolha = JOptionPane.showConfirmDialog(null, "Deseja remover alguma disciplina ? ");
+
+        if (escolha ==0){
+            String disciplinaRemover = JOptionPane.showInputDialog("Qual a disciplina 1, 2, 3 ou 4 ?" );
+            aluno1.getDisciplinas().remove(Integer.valueOf(disciplinaRemover) -1);
         }
 
 
